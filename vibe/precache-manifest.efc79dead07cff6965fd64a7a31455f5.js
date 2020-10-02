84a2438b6a37d51228c1b01564de4687138fc5ee
cammyleone <cammy@willyouplease.me>
2020-10-01 22:35:34 -0700

    vibe: first deploy

---
+++ vibe/precache-manifest.efc79dead07cff6965fd64a7a31455f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32d37815a87850c3d0fb744380f1099",
+    "url": "https://plug.willyouplease.me/vibe/index.html"
+  },
+  {
+    "revision": "dbda656901a3bae21f6f",
+    "url": "https://plug.willyouplease.me/vibe/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "557d3181891947069a35",
+    "url": "https://plug.willyouplease.me/vibe/static/js/2.f90a6ee4.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "https://plug.willyouplease.me/vibe/static/js/2.f90a6ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbda656901a3bae21f6f",
+    "url": "https://plug.willyouplease.me/vibe/static/js/main.bbb955ed.chunk.js"
+  },
+  {
+    "revision": "84a89b5f7b2c74330aa2",
+    "url": "https://plug.willyouplease.me/vibe/static/js/runtime-main.f3d516b4.js"
+  }
+]);
