fac0a7d167cbfa2997ac0a525c2aa11f7817ac76
cammyleone <cammy@willyouplease.me>
2020-10-02 12:13:57 -0700

    vibe: reverting - pointing back at https

--- vibe/precache-manifest.efc79dead07cff6965fd64a7a31455f5.js
+++ vibe/precache-manifest.efc79dead07cff6965fd64a7a31455f5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cd5a46ef20e754cf871ffc9347b191e2",
+    "revision": "e32d37815a87850c3d0fb744380f1099",
     "url": "https://plug.willyouplease.me/vibe/index.html"
   },
   {
-    "revision": "6cb92bbda2dcc49a2e38",
+    "revision": "dbda656901a3bae21f6f",
     "url": "https://plug.willyouplease.me/vibe/static/css/main.69eda509.chunk.css"
   },
   {
-    "revision": "cb3a3b70a559f96c4395",
-    "url": "https://plug.willyouplease.me/vibe/static/js/2.2bc63d00.chunk.js"
+    "revision": "557d3181891947069a35",
+    "url": "https://plug.willyouplease.me/vibe/static/js/2.f90a6ee4.chunk.js"
   },
   {
     "revision": "b67bc97fd0dd67004287af4028afc1b1",
-    "url": "https://plug.willyouplease.me/vibe/static/js/2.2bc63d00.chunk.js.LICENSE.txt"
+    "url": "https://plug.willyouplease.me/vibe/static/js/2.f90a6ee4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6cb92bbda2dcc49a2e38",
-    "url": "https://plug.willyouplease.me/vibe/static/js/main.8818298e.chunk.js"
+    "revision": "dbda656901a3bae21f6f",
+    "url": "https://plug.willyouplease.me/vibe/static/js/main.bbb955ed.chunk.js"
   },
   {
     "revision": "84a89b5f7b2c74330aa2",
