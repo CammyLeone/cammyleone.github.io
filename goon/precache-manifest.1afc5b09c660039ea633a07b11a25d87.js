3c297f06ef9dd2706eadb799e0b3277033e9caa7
cammyleone <cammy@willyouplease.me>
2021-06-08 23:18:34 -0400

    goon: first deploy

---
+++ goon/precache-manifest.1afc5b09c660039ea633a07b11a25d87.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0b703d5ca648fdcbc6f87cbc263986b",
+    "url": "https://plug.willyouplease.me/goon/index.html"
+  },
+  {
+    "revision": "da6684d1e55e34d11bff",
+    "url": "https://plug.willyouplease.me/goon/static/css/2.f26d8bb1.chunk.css"
+  },
+  {
+    "revision": "da6684d1e55e34d11bff",
+    "url": "https://plug.willyouplease.me/goon/static/js/2.ee1b1e0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://plug.willyouplease.me/goon/static/js/2.ee1b1e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9acabe4a8cf6970c84c",
+    "url": "https://plug.willyouplease.me/goon/static/js/main.0ac09f7b.chunk.js"
+  },
+  {
+    "revision": "92c1420b9186ebe76544",
+    "url": "https://plug.willyouplease.me/goon/static/js/runtime-main.36afef3c.js"
+  },
+  {
+    "revision": "47e38a2c36aa9b20f95e1b2cd3399405",
+    "url": "https://plug.willyouplease.me/goon/static/media/monoton-all-400-normal.47e38a2c.woff"
+  },
+  {
+    "revision": "a159af3f5a12afbfa89e24f94586b10b",
+    "url": "https://plug.willyouplease.me/goon/static/media/monoton-latin-400-normal.a159af3f.woff2"
+  },
+  {
+    "revision": "7b7c08cbb3290677215223b80fd23e2b",
+    "url": "https://plug.willyouplease.me/goon/static/media/paytone-one-all-400-normal.7b7c08cb.woff"
+  },
+  {
+    "revision": "fa9b1a65dd0724a4757da8613a93f295",
+    "url": "https://plug.willyouplease.me/goon/static/media/paytone-one-latin-400-normal.fa9b1a65.woff2"
+  },
+  {
+    "revision": "d3b41ee222c485912141bd56ac3cdde6",
+    "url": "https://plug.willyouplease.me/goon/static/media/paytone-one-latin-ext-400-normal.d3b41ee2.woff2"
+  },
+  {
+    "revision": "6fdfae29623b6bc61c4ac156d1910f3d",
+    "url": "https://plug.willyouplease.me/goon/static/media/paytone-one-vietnamese-400-normal.6fdfae29.woff2"
+  }
+]);
