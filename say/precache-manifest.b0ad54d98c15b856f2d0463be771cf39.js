d681adc2bb95a078d59f729a49efd8ced2432f85
cammyleone <cammy@willyouplease.me>
2021-06-13 18:34:54 -0400

    say: after rewriting history and replacing env files

---
+++ say/precache-manifest.b0ad54d98c15b856f2d0463be771cf39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4eff302287f552f1415371b5e627a7",
+    "url": "https://plug.willyouplease.me/say/index.html"
+  },
+  {
+    "revision": "40ec08424fbfa652ec67",
+    "url": "https://plug.willyouplease.me/say/static/css/main.aec53aa3.chunk.css"
+  },
+  {
+    "revision": "507027f42353f2c3cf7a",
+    "url": "https://plug.willyouplease.me/say/static/js/2.a651cca9.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "https://plug.willyouplease.me/say/static/js/2.a651cca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ec08424fbfa652ec67",
+    "url": "https://plug.willyouplease.me/say/static/js/main.6ab1b878.chunk.js"
+  },
+  {
+    "revision": "705663d4c5a5879e6731",
+    "url": "https://plug.willyouplease.me/say/static/js/runtime-main.8365ed97.js"
+  },
+  {
+    "revision": "6f562c58cfb2cd4c6a6fe214c1903ae1",
+    "url": "https://plug.willyouplease.me/say/static/media/chalk.6f562c58.png"
+  }
+]);
--- say/service-worker.js
+++ say/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b97366b9ff5b4a7e62ffe012ec45121.js"
+  "https://plug.willyouplease.me/say/precache-manifest.b0ad54d98c15b856f2d0463be771cf39.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://plug.willyouplease.me/say/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
