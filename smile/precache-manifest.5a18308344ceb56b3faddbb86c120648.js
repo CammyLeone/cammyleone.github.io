e82ac479542f1d208fd1c59afe2097ef226f3443
Cammy Leone <cammy@willyouplease.me>
2021-11-24 22:20:47 -0800

    smile: v0

---
+++ smile/precache-manifest.5a18308344ceb56b3faddbb86c120648.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba9f0b988666a816358911ec3c7a762",
+    "url": "https://plug.willyouplease.me/smile/index.html"
+  },
+  {
+    "revision": "97e3ea2944ea16a38b4d",
+    "url": "https://plug.willyouplease.me/smile/static/css/2.9cd0e5bb.chunk.css"
+  },
+  {
+    "revision": "97e3ea2944ea16a38b4d",
+    "url": "https://plug.willyouplease.me/smile/static/js/2.5bb9f6d3.chunk.js"
+  },
+  {
+    "revision": "7bc50d6463d642c6f6f72f8a13677fb5",
+    "url": "https://plug.willyouplease.me/smile/static/js/2.5bb9f6d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce505efd80f2e8e76760",
+    "url": "https://plug.willyouplease.me/smile/static/js/main.74a042f3.chunk.js"
+  },
+  {
+    "revision": "a9c6769bd742e1da1ea1",
+    "url": "https://plug.willyouplease.me/smile/static/js/runtime-main.ab14e399.js"
+  },
+  {
+    "revision": "b9e3b5b486da4033b6f9353960774931",
+    "url": "https://plug.willyouplease.me/smile/static/media/sigmar-one-all-400-normal.b9e3b5b4.woff"
+  },
+  {
+    "revision": "75fb6e4b77343e10201e2a949a7e9f67",
+    "url": "https://plug.willyouplease.me/smile/static/media/sigmar-one-latin-400-normal.75fb6e4b.woff2"
+  },
+  {
+    "revision": "33db2ca832956fc91612be40f40524b5",
+    "url": "https://plug.willyouplease.me/smile/static/media/sigmar-one-latin-ext-400-normal.33db2ca8.woff2"
+  },
+  {
+    "revision": "81643e4ef0ca2b9669761a50885a9a9c",
+    "url": "https://plug.willyouplease.me/smile/static/media/sigmar-one-vietnamese-400-normal.81643e4e.woff2"
+  }
+]);
